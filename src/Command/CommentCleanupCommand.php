9169b41099c3104476ea6d1fc55d57fccc38b285
Maxim Semkin <semkin.maksim@mail.ru>
2023-04-17 21:59:46 +0300

    Deleted spam comments

---
+++ src/Command/CommentCleanupCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Command;
+
+use App\Repository\CommentRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:comment:cleanup',
+    description: 'Deletes rejected and spam comments from the database',
+)]
+class CommentCleanupCommand extends Command
+{
+    public function __construct(
+        private readonly CommentRepository $commentRepository,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addOption('dry-run', null, InputOption::VALUE_NONE, 'Dry run')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        if ($input->getOption('dry-run')) {
+            $io->note('Dry mode enabled');
+
+            $count = $this->commentRepository->countOldRejected();
+        } else {
+            $count = $this->commentRepository->deleteOldRejected();
+        }
+
+        $io->success(sprintf('Deleted "%d" old rejected/spam comments.', $count));
+
+        return Command::SUCCESS;
+    }
+}
