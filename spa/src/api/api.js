92460759cabb0d0f0ad7ed70785f545426b93962
Maxim Semkin <semkin.maksim@mail.ru>
2023-04-22 23:25:46 +0300

    Add the SPA application

---
+++ spa/src/api/api.js
@@ -0,0 +1,12 @@
+function fetchCollection(path) {
+    return fetch(ENV_API_ENDPOINT + path).then(resp => resp.json()).then()
+        .then(json => json['hydra:member']);
+}
+
+export function findConferences() {
+    return fetchCollection('api/conferences');
+}
+
+export function findComments(conference) {
+    return fetchCollection('api/comments?conference='+conference.id);
+}
